c3e269ffcd17b4016d9f565aa256420cee7c0b56
edmundo papi <edmundopierre@yahoo.com>
2020-12-27 12:23:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+const map = function(arr, fn{
+  return fn(arr)
+}
+
+let fn = function(arr){
+  let empty = []
+  arr.forEach(ar =>{
+    return (empty.push(ar*-1));
+  })
+  return empty;
+}
